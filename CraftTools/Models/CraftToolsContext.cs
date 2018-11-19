d5b8f2b6c08672be1454503c885f6efd197f9990
Max <maximrimkus@yandex.by>
2018-11-19 15:28:13 +0300

    CraftToolsContext

---
+++ CraftTools/Models/CraftToolsContext.cs
@@ -0,0 +1,19 @@
+﻿namespace CraftTools.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class CraftToolsContext : DbContext
+    {
+        public CraftToolsContext()
+            : base("name=CraftToolsContext")
+        {
+        }
+
+        DbSet<Material> Materials { get; set; }
+        DbSet<Ware> Wares { get; set; }
+        DbSet<Profit> Profits { get; set; }
+        DbSet<Loss> Losses { get; set; }
+    }
+}
