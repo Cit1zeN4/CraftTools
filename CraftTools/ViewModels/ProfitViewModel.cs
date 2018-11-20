b20927be4e80def8e688484cfd808575ae528aa2
Max <maximrimkus@yandex.by>
2018-11-20 22:29:38 +0300

    Added ProfitView & ProfitViewModel

---
+++ CraftTools/ViewModels/ProfitViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CraftTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftTools.ViewModels
+{
+    public class ProfitViewModel
+    {
+        CraftToolsContext context;
+
+        #region Constructors
+
+        public ProfitViewModel()
+        {
+            Profits = new ObservableCollection<Profit>();
+
+            using (context = new CraftToolsContext())
+            {
+                foreach(Profit p in context.Profits)
+                {
+                    Profits.Add(p);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<Profit> Profits { get; set; }
+
+        #endregion
+    }
+}
--- CraftTools/Views/ProfitView.xaml.cs
+++ CraftTools/Views/ProfitView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CraftTools
+namespace CraftTools.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ProfitView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ProfitView : UserControl
     {
-        public MainWindow()
+        public ProfitView()
         {
             InitializeComponent();
         }
