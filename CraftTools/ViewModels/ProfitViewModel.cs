59ea878f40297e55f1ff2812ee490bf02a839c20
Max <maximrimkus@yandex.by>
2018-11-23 18:05:39 +0300

    Added ProfitEditBox in ProfitView

--- CraftTools/ViewModels/ProfitViewModel.cs
+++ CraftTools/ViewModels/ProfitViewModel.cs
@@ -2,15 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CraftTools.ViewModels
 {
-    public class ProfitViewModel
+    public class ProfitViewModel : INotifyPropertyChanged
     {
-        CraftToolsContext context;
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, e);
+        }
+
+        #endregion
 
         #region Constructors
 
@@ -18,9 +34,9 @@ namespace CraftTools.ViewModels
         {
             Profits = new ObservableCollection<Profit>();
 
-            using (context = new CraftToolsContext())
+            using (_context = new CraftToolsContext())
             {
-                foreach(Profit p in context.Profits)
+                foreach(Profit p in _context.Profits)
                 {
                     Profits.Add(p);
                 }
@@ -31,11 +47,30 @@ namespace CraftTools.ViewModels
 
         #region Fields
 
+        CraftToolsContext _context;
+        Profit _selectedProfit;
+
         #endregion
 
         #region Properties
 
         public ObservableCollection<Profit> Profits { get; set; }
+
+        public Profit SelectedProfit
+        {
+            get
+            {
+                if (_selectedProfit == null)
+                    return null;
+                else
+                    return _selectedProfit;
+            }
+            set
+            {
+                _selectedProfit = value;
+                OnPropertyChanged();
+            }
+        }
 
         #endregion
     }
